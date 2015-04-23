9cc7e3479b8bb4c46933c117d2d18846f658dcd4
philliphuang <philliphuang@college.harvard.edu>
2015-04-23 16:08:58 -0400

    lightblue is working

--- lightblue.py
+++ lightblue.py
@@ -1,11 +1,20 @@
 import parser
 import chess as ch
+import graph
 
 print "Welcome to Light Blue!"
+print "Type quit at any time to quit the application."
 
-graph.load("database")
+graph.initialize()
+graph.load("Carlsen20")
 
-quit = false
+current_game = ch.Game()
+current_game.setup()
+color = 'w'
+quit = False
+
+print "Here is the board:"
+print current_game.board
 
 # first recommend
 lst = graph.firstrecommend()
@@ -17,7 +26,7 @@ if lstlen == 1:
 	print y
 elif lstlen == 0:
 	print "Error: No Data"
-	quit = true
+	quit = True
 else:
 	print "You should make one of these moves:"
 	for item in lst:
@@ -26,32 +35,32 @@ else:
 		print "It should give you:"
 		print y
 
-current_game = ch.Game()
-current_game.setup()
-
-color = 'w'
-
-while quit == false:
-	mv = raw_input("What is your move?")
-	if mv = "quit":
+while quit == False:
+	mv = raw_input("What is your move? ")
+	if mv == "quit":
 		break
 	before = str(current_game.board)
 	current_game.board.move(mv)
 	after = str(current_game.board)
-	if color = 'w':
+	if color == 'w':
 		color = 'b'
 	else: color = 'w'
 	lst2 = graph.recommend(before,mv,color,after)
 	lst2len = len(lst2)
 	if lst2len == 1:
+		print "Here is the board:"
+		print after
 		(x,y) = lst2[0]
 		print "You should make this move:", x
 		print "It should give you:"
 		print y
 	elif lst2len == 0:
-		print "Error: No Data"
-		quit = true
+		print "No Data: Make a random move"
+		print "Here is the board:"
+		print after
 	else:
+		print "Here is the board:"
+		print after
 		print "You should make one of these moves:"
 		for item in lst2:
 			(x,y) = lst2[item]
