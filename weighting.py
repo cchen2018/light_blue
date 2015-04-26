144ab319d3b81b53b26e88103988d9177353c8d9
philliphuang <philliphuang@college.harvard.edu>
2015-04-26 15:19:00 -0400

    graph and weighting fixes

--- weighting.py
+++ weighting.py
@@ -1,5 +1,7 @@
 # getting ready to implement elo and win/loss
 
+from __future__ import division
+
 class popularity(object):
 
 	# iterates popularity by one
@@ -10,7 +12,10 @@ class popularity(object):
 
 class elo(object):
 	def __init__(self, new):
+		if new == 0:
+			new = 2000
 		self.new = new 
+		self.default = (new,)
 
     # overrides with higher elo
 	def alter(self, current):
@@ -18,16 +23,25 @@ class elo(object):
 		if self.new > rating:
 			return (self.new,)
 		else: return current
-	default = (1,)
 
 class winloss(object):
 	def __init__(self, new):
 		self.new = new 
 
-    # alters win/loss ratio
-	def alter(self, current):
-		(weight, win, loss) = current
 		# tie
+		if self.new == "t":
+			self.default = (1,1,1)
+		# win
+		elif self.new == "w":
+			self.default = (2,2,1)
+		# loss
+		else:
+			self.default = (0,0,1)
+
+	def alter(self, current):
+		(weight,win,loss) = current
+
+		#tie
 		if self.new == "t":
 			return current
 		# win
@@ -38,6 +52,6 @@ class winloss(object):
 		else:
 			loss += 1
 			return ((win/loss),win,loss)
-	default = (1,1,1)
+
 
 
