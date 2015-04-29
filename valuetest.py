4b522274921e7507741cc8bcc9512ac094265aaa
philliphuang <philliphuang@college.harvard.edu>
2015-04-29 10:09:12 -0400

    fixed valuetest

--- valuetest.py
+++ valuetest.py
@@ -5,7 +5,7 @@ import chess as ch
 import sys
 
 weight_type = sys.argv[1]
-graph.load("Carlsen20" + weight_type)
+graph.load("Carlsen100" + weight_type)
 starting_game = ch.Game()
 starting_game.setup()
 startpos = str(starting_game.board)
